528ca831eaf1ee64e7bdea4622bad82ccf140b56
BIZONANCE <bizonance@gmail.com>
2026-05-11 13:33:13 +0530

    changes

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,17 +2,16 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.DATABASE_URL?.includes("localhost")
+    ? false
+    : { rejectUnauthorized: false },
 });
 
-// Optional: test connection once (not required, but useful)
 export const connectDB = async () => {
   try {
     const client = await pool.connect();
     client.release();
-    console.log("✅ Neon PostgreSQL connected (POOLING)");
+    console.log("✅ PostgreSQL connected");
   } catch (error) {
     console.error("❌ Database connection failed", error);
     process.exit(1);
